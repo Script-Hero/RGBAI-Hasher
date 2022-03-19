deacc732db657c6c81ecd94b354cc7633b538daf
Neil Aylor <neilaylor@hotmail.com>
2022-03-18 20:29:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let md5 = require('md5');
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      "input":""
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.validate = this.validate.bind(this);
+  }
+
+  onChange(e){
+    this.setState({input:e.target.value})
+  }
+
+  validate(){
+    // must be all integer, 6 length
+    let isnum = /^\d+$/.test(this.state.input); // test if all integers
+
+    return (isnum && this.state.input.length == 6);
+  }
+
+  render(){
+    return (
+      <div id="body">
+        <div id="input-box-container"><input id='input-box' placeholder='Enter your Student ID' onChange={this.onChange}></input></div>
+
+        <div id="output-container">
+          <h1 id="output" tooltip='Click to copy' onClick={() => {navigator.clipboard.writeText(md5(this.state.input))}}>{this.validate() ? md5(this.state.input) : ""}</h1>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
